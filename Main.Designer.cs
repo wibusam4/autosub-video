5837d9edb1a3f5b000a001b17a60a54e4076ce35
Zen0sama <wibusama1998@gmail.com>
2023-04-20 15:58:25 +0700

    first commit

---
+++ Main.Designer.cs
@@ -0,0 +1,314 @@
+﻿
+namespace AutoSub
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.siticonePanel1 = new ns1.SiticonePanel();
+            this.btnMiniSize = new ns1.SiticoneButton();
+            this.btnExit = new ns1.SiticoneButton();
+            this.siticoneLabel1 = new ns1.SiticoneLabel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.siticoneLabel5 = new ns1.SiticoneLabel();
+            this.siticonePanel3 = new ns1.SiticonePanel();
+            this.siticoneLabel2 = new ns1.SiticoneLabel();
+            this.siticonePanel2 = new ns1.SiticonePanel();
+            this.btnGetPathVideo = new ns1.SiticoneButton();
+            this.txtPathVideo = new ns1.SiticoneTextBox();
+            this.siticoneLabel3 = new ns1.SiticoneLabel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.siticonePictureBox1 = new ns1.SiticonePictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.siticonePanel3.SuspendLayout();
+            this.siticonePanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.siticonePictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // siticonePanel1
+            // 
+            this.siticonePanel1.BackColor = System.Drawing.Color.White;
+            this.siticonePanel1.Location = new System.Drawing.Point(0, 0);
+            this.siticonePanel1.Name = "siticonePanel1";
+            this.siticonePanel1.ShadowDecoration.Parent = this.siticonePanel1;
+            this.siticonePanel1.Size = new System.Drawing.Size(1700, 5);
+            this.siticonePanel1.TabIndex = 0;
+            // 
+            // btnMiniSize
+            // 
+            this.btnMiniSize.BorderColor = System.Drawing.Color.White;
+            this.btnMiniSize.BorderRadius = 12;
+            this.btnMiniSize.BorderThickness = 2;
+            this.btnMiniSize.CheckedState.Parent = this.btnMiniSize;
+            this.btnMiniSize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMiniSize.CustomImages.Parent = this.btnMiniSize;
+            this.btnMiniSize.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(43)))));
+            this.btnMiniSize.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMiniSize.ForeColor = System.Drawing.Color.White;
+            this.btnMiniSize.HoveredState.Parent = this.btnMiniSize;
+            this.btnMiniSize.Location = new System.Drawing.Point(1634, 11);
+            this.btnMiniSize.Name = "btnMiniSize";
+            this.btnMiniSize.ShadowDecoration.Parent = this.btnMiniSize;
+            this.btnMiniSize.Size = new System.Drawing.Size(26, 26);
+            this.btnMiniSize.TabIndex = 1;
+            this.btnMiniSize.Text = "-";
+            this.btnMiniSize.Click += new System.EventHandler(this.btnMiniSize_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BorderColor = System.Drawing.Color.White;
+            this.btnExit.BorderRadius = 12;
+            this.btnExit.BorderThickness = 2;
+            this.btnExit.CheckedState.Parent = this.btnExit;
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.CustomImages.Parent = this.btnExit;
+            this.btnExit.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(43)))));
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.HoveredState.Parent = this.btnExit;
+            this.btnExit.Location = new System.Drawing.Point(1670, 11);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.ShadowDecoration.Parent = this.btnExit;
+            this.btnExit.Size = new System.Drawing.Size(26, 26);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "X";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // siticoneLabel1
+            // 
+            this.siticoneLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneLabel1.ForeColor = System.Drawing.Color.White;
+            this.siticoneLabel1.Location = new System.Drawing.Point(12, 10);
+            this.siticoneLabel1.Name = "siticoneLabel1";
+            this.siticoneLabel1.Size = new System.Drawing.Size(182, 27);
+            this.siticoneLabel1.TabIndex = 3;
+            this.siticoneLabel1.Text = "Auto Sub Pro v1,0,0";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 63);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1676, 525);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(43)))));
+            this.tabPage1.Controls.Add(this.siticoneLabel5);
+            this.tabPage1.Controls.Add(this.siticonePanel3);
+            this.tabPage1.Controls.Add(this.siticoneLabel2);
+            this.tabPage1.Controls.Add(this.siticonePanel2);
+            this.tabPage1.ForeColor = System.Drawing.Color.White;
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1668, 497);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tách Sub";
+            // 
+            // siticoneLabel5
+            // 
+            this.siticoneLabel5.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel5.Location = new System.Drawing.Point(676, 3);
+            this.siticoneLabel5.MinimumSize = new System.Drawing.Size(70, 17);
+            this.siticoneLabel5.Name = "siticoneLabel5";
+            this.siticoneLabel5.Size = new System.Drawing.Size(70, 17);
+            this.siticoneLabel5.TabIndex = 3;
+            this.siticoneLabel5.Text = "Video";
+            this.siticoneLabel5.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // siticonePanel3
+            // 
+            this.siticonePanel3.BorderColor = System.Drawing.Color.White;
+            this.siticonePanel3.BorderRadius = 5;
+            this.siticonePanel3.BorderThickness = 1;
+            this.siticonePanel3.Controls.Add(this.siticonePictureBox1);
+            this.siticonePanel3.ForeColor = System.Drawing.Color.White;
+            this.siticonePanel3.Location = new System.Drawing.Point(653, 10);
+            this.siticonePanel3.Name = "siticonePanel3";
+            this.siticonePanel3.ShadowDecoration.Parent = this.siticonePanel3;
+            this.siticonePanel3.Size = new System.Drawing.Size(622, 321);
+            this.siticonePanel3.TabIndex = 1;
+            // 
+            // siticoneLabel2
+            // 
+            this.siticoneLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel2.Location = new System.Drawing.Point(30, 3);
+            this.siticoneLabel2.MinimumSize = new System.Drawing.Size(70, 17);
+            this.siticoneLabel2.Name = "siticoneLabel2";
+            this.siticoneLabel2.Size = new System.Drawing.Size(70, 17);
+            this.siticoneLabel2.TabIndex = 0;
+            this.siticoneLabel2.Text = "Cài Đặt";
+            this.siticoneLabel2.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // siticonePanel2
+            // 
+            this.siticonePanel2.BorderColor = System.Drawing.Color.White;
+            this.siticonePanel2.BorderRadius = 5;
+            this.siticonePanel2.BorderThickness = 1;
+            this.siticonePanel2.Controls.Add(this.btnGetPathVideo);
+            this.siticonePanel2.Controls.Add(this.txtPathVideo);
+            this.siticonePanel2.Controls.Add(this.siticoneLabel3);
+            this.siticonePanel2.ForeColor = System.Drawing.Color.Black;
+            this.siticonePanel2.Location = new System.Drawing.Point(10, 10);
+            this.siticonePanel2.Name = "siticonePanel2";
+            this.siticonePanel2.ShadowDecoration.Parent = this.siticonePanel2;
+            this.siticonePanel2.Size = new System.Drawing.Size(622, 321);
+            this.siticonePanel2.TabIndex = 0;
+            // 
+            // btnGetPathVideo
+            // 
+            this.btnGetPathVideo.BorderColor = System.Drawing.Color.White;
+            this.btnGetPathVideo.BorderRadius = 5;
+            this.btnGetPathVideo.BorderThickness = 1;
+            this.btnGetPathVideo.CheckedState.Parent = this.btnGetPathVideo;
+            this.btnGetPathVideo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGetPathVideo.CustomImages.Parent = this.btnGetPathVideo;
+            this.btnGetPathVideo.FillColor = System.Drawing.Color.Transparent;
+            this.btnGetPathVideo.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnGetPathVideo.ForeColor = System.Drawing.Color.White;
+            this.btnGetPathVideo.HoveredState.Parent = this.btnGetPathVideo;
+            this.btnGetPathVideo.Image = global::AutoSub.Properties.Resources.folder1;
+            this.btnGetPathVideo.ImageOffset = new System.Drawing.Point(1, 0);
+            this.btnGetPathVideo.ImageSize = new System.Drawing.Size(15, 15);
+            this.btnGetPathVideo.Location = new System.Drawing.Point(548, 38);
+            this.btnGetPathVideo.Name = "btnGetPathVideo";
+            this.btnGetPathVideo.ShadowDecoration.Parent = this.btnGetPathVideo;
+            this.btnGetPathVideo.Size = new System.Drawing.Size(23, 23);
+            this.btnGetPathVideo.TabIndex = 2;
+            this.btnGetPathVideo.Click += new System.EventHandler(this.btnGetPathVideo_Click);
+            // 
+            // txtPathVideo
+            // 
+            this.txtPathVideo.BorderRadius = 2;
+            this.txtPathVideo.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPathVideo.DefaultText = "";
+            this.txtPathVideo.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtPathVideo.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtPathVideo.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtPathVideo.DisabledState.Parent = this.txtPathVideo;
+            this.txtPathVideo.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtPathVideo.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtPathVideo.FocusedState.Parent = this.txtPathVideo;
+            this.txtPathVideo.ForeColor = System.Drawing.Color.Black;
+            this.txtPathVideo.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtPathVideo.HoveredState.Parent = this.txtPathVideo;
+            this.txtPathVideo.Location = new System.Drawing.Point(143, 39);
+            this.txtPathVideo.Name = "txtPathVideo";
+            this.txtPathVideo.PasswordChar = '\0';
+            this.txtPathVideo.PlaceholderText = "";
+            this.txtPathVideo.SelectedText = "";
+            this.txtPathVideo.ShadowDecoration.Parent = this.txtPathVideo;
+            this.txtPathVideo.Size = new System.Drawing.Size(399, 20);
+            this.txtPathVideo.TabIndex = 1;
+            // 
+            // siticoneLabel3
+            // 
+            this.siticoneLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel3.ForeColor = System.Drawing.Color.White;
+            this.siticoneLabel3.Location = new System.Drawing.Point(40, 40);
+            this.siticoneLabel3.Name = "siticoneLabel3";
+            this.siticoneLabel3.Size = new System.Drawing.Size(88, 15);
+            this.siticoneLabel3.TabIndex = 0;
+            this.siticoneLabel3.Text = "Đường dẫn video:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(43)))));
+            this.tabPage2.ForeColor = System.Drawing.Color.White;
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1668, 497);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Tài Khoản";
+            // 
+            // siticonePictureBox1
+            // 
+            this.siticonePictureBox1.Location = new System.Drawing.Point(23, 30);
+            this.siticonePictureBox1.Name = "siticonePictureBox1";
+            this.siticonePictureBox1.ShadowDecoration.Parent = this.siticonePictureBox1;
+            this.siticonePictureBox1.Size = new System.Drawing.Size(579, 244);
+            this.siticonePictureBox1.TabIndex = 0;
+            this.siticonePictureBox1.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(31)))), ((int)(((byte)(43)))));
+            this.ClientSize = new System.Drawing.Size(1700, 649);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.siticoneLabel1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnMiniSize);
+            this.Controls.Add(this.siticonePanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Sub Pro";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.siticonePanel3.ResumeLayout(false);
+            this.siticonePanel2.ResumeLayout(false);
+            this.siticonePanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.siticonePictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ns1.SiticonePanel siticonePanel1;
+        private ns1.SiticoneButton btnMiniSize;
+        private ns1.SiticoneButton btnExit;
+        private ns1.SiticoneLabel siticoneLabel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private ns1.SiticonePanel siticonePanel2;
+        private ns1.SiticoneLabel siticoneLabel2;
+        private ns1.SiticoneTextBox txtPathVideo;
+        private ns1.SiticoneLabel siticoneLabel3;
+        private ns1.SiticoneButton btnGetPathVideo;
+        private ns1.SiticonePanel siticonePanel3;
+        private ns1.SiticoneLabel siticoneLabel5;
+        private ns1.SiticonePictureBox siticonePictureBox1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,61 @@
+﻿using AForge.Video.FFMPEG;
+using System;
+using System.Windows.Forms;
+namespace AutoSub
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMiniSize_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnGetPathVideo_Click(object sender, EventArgs e)
+        {
+            GetPathVideo();
+        }
+
+        private void GetPathVideo()
+        {
+            using (OpenFileDialog file = new OpenFileDialog())
+            {
+                file.InitialDirectory = "c:\\";
+                file.Filter = "mp4 files (*.mp4)|*.mp4|All files (*.*)|*.*";
+                file.FilterIndex = 2;
+                file.RestoreDirectory = true;
+
+                if (file.ShowDialog() == DialogResult.OK)
+                {
+                    GetSub.PathVideo = file.FileName;
+                    txtPathVideo.Text = file.FileName;
+                }
+            }
+            if(!string.IsNullOrEmpty(GetSub.PathVideo))
+            {
+                using (var video = new VideoFileReader())
+                {
+                    video.Open(GetSub.PathVideo);
+
+                    // Lấy số khung hình trên mỗi giây (frame rate) của video
+                    double frameRate = video.FrameRate;
+
+                    MessageBox.Show(frameRate.ToString());
+
+                    video.Close();
+                }
+            }
+
+            
+
+        }
+    }
+}
